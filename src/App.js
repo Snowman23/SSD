12f199df9dc7ccd75fc405f8ccd1e342aa90864f
Flavius <33982750+Snowman23@users.noreply.github.com>
2025-11-16 19:09:25 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./Login";
 import Dashboard from "./pages/Dashboard";
 
@@ -6,9 +7,15 @@ function App() {
   const [user, setUser] = useState(null);
 
   return (
-    <div>
-      {!user ? <Login onLogin={setUser} /> : <Dashboard user={user} />}
-    </div>
+    <Router>
+      <Routes>
+        <Route
+          path="/"
+          element={!user ? <Login onLogin={setUser} /> : <Dashboard user={user} />}
+        />
+        {/* Future routes can go here */}
+      </Routes>
+    </Router>
   );
 }
 
